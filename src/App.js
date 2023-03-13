3300044324c247ba8fa5c835c5b0a812f471f3df
Eclk Çolak <eclkcolak@Eclks-MacBook-Pro.local>
2023-03-13 12:51:55 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [wallet,setWallet] = useState('');
+	const [step,setStep] = useState(1);
+	const [code,setCode] = useState('');
+	const [status,setStatus] = useState('');
+	const checkWallet = () => {
+		console.log(wallet);
+		setStep(2);
+		setCode('test-'+Math.random());
+	}
+
+	const checkCode = () => {
+		fetch("https://opensea.io/"+wallet)
+		.then((response) => response.text())
+		.then((data) => {
+			if(data.indexOf(code)>-1){
+				setStatus('Onaylandı');
+			}else{
+				setStatus('Onaylanmadı');
+			}
+		});
+	}
+
+	return (
+		<div className="App">
+			{step == 1 ?
+				<>
+					<input type="text"
+						value={wallet}
+						onChange={(event) => {
+							setWallet(event.target.value);
+						}}
+					></input>
+					<button onClick={checkWallet}>Cüzdan Gir</button>
+				</>
+			: null}
+			{step == 2 ?
+				<>
+					<h1>{code}</h1>
+					<h2>{status}</h2>
+					<button onClick={checkCode}>Onayla</button>
+				</>
+			: null}
+			
+		</div>
+	);
 }
 
 export default App;
